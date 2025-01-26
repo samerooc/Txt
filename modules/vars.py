42eca7ac45f30c41f24e4387f126d27dd0808de6
samerooc <roocrolex@gmail.com>
2025-01-26 14:54:58 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28929701"
-api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
-bot_token = "6893027676:AAEytAdJL-5uY_2LqeCVWA7ziogwPvxBZQw"
+api_id = "26343810"
+api_hash = "6ac64c03481d679241dd749b8ecee201"
+bot_token = "7924569463:AAH6e-y5CE8Z_iTXM_-fL_AREBg5pHQ2ihg"
